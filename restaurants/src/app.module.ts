17403203e9aa140bae9c75ec5d93efe8b4d47dbb
EduardoAmaral <eduardofsamaral@gmail.com>
2021-07-19 19:18:11 -0300

    [Restaurants] Configure k8s deploy and services

--- restaurants/src/app.module.ts
+++ restaurants/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [RestaurantsModule],
+  imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
