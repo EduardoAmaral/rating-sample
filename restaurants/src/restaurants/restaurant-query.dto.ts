a00eb58638edb0c114671ab4f3fac24823b72ce6
EduardoAmaral <eduardofsamaral@gmail.com>
2021-07-19 17:16:09 -0300

    [Restaurants] Validate requests fields on search dto

--- restaurants/src/restaurants/restaurant-query.dto.ts
+++ restaurants/src/restaurants/restaurant-query.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { RestaurantStatus } from './restaurant.model';
 
 export class RestaurantQueryDto {
+  @IsOptional()
+  @IsString()
   search?: string;
+
+  @IsOptional()
+  @IsEnum(RestaurantStatus)
   status?: RestaurantStatus;
 }
