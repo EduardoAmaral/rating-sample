da01438428a3a9c58d77467556396742091aad92
EduardoAmaral <eduardofsamaral@gmail.com>
2021-07-24 13:31:53 -0300

    [Restaurants] Add unit tests

--- restaurants/src/restaurants/__test__/restaurants.controller.spec.ts
+++ restaurants/src/restaurants/__test__/restaurants.controller.spec.ts
@@ -1,18 +1,160 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Restaurant } from '../model/restautant.entity';
+import { RestaurantStatus } from '../restaurant.status';
 import { RestaurantsController } from '../restaurants.controller';
+import { RestaurantsService } from '../restaurants.service';
+
+const mockRestaurantsService = () => ({
+  create: jest.fn(),
+  searchBy: jest.fn(),
+  getById: jest.fn(),
+  deleteById: jest.fn(),
+  updateStatus: jest.fn(),
+});
 
 describe('RestaurantsController', () => {
   let controller: RestaurantsController;
+  let service: RestaurantsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RestaurantsController],
+      providers: [
+        { provide: RestaurantsService, useFactory: mockRestaurantsService },
+      ],
     }).compile();
 
     controller = module.get<RestaurantsController>(RestaurantsController);
+    service = module.get<RestaurantsService>(RestaurantsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a restaurant', async () => {
+    const name = 'Restaurant';
+    const description = 'Some description';
+    jest.spyOn(service, 'create').mockResolvedValueOnce({
+      id: 'uuid',
+      name,
+      description,
+      status: RestaurantStatus.CLOSED,
+      created: new Date(),
+      updated: new Date(),
+    });
+
+    const result: Restaurant = await controller.create({
+      name,
+      description,
+    });
+
+    expect(service.create).toHaveBeenCalledWith({ name, description });
+
+    expect(result.id).toBe('uuid');
+    expect(result.name).toBe(name);
+    expect(result.description).toBe(description);
+    expect(result.status).toBe(RestaurantStatus.CLOSED);
+  });
+
+  it('should get all restaurants', async () => {
+    const now = new Date();
+    jest.spyOn(service, 'searchBy').mockResolvedValueOnce([
+      {
+        id: 'uuid 1',
+        name: 'Restaurant 1',
+        description: 'Desc 1',
+        status: RestaurantStatus.OPEN,
+        created: now,
+        updated: now,
+      },
+      {
+        id: 'uuid 2',
+        name: 'Restaurant 2',
+        description: 'Desc 2',
+        status: RestaurantStatus.CLOSED,
+        created: now,
+        updated: now,
+      },
+    ]);
+
+    const result = await controller.getAll({ search: 'Rest' });
+
+    expect(service.searchBy).toHaveBeenLastCalledWith({ search: 'Rest' });
+
+    expect(result).toHaveLength(2);
+    expect(result).toContainEqual({
+      id: 'uuid 1',
+      name: 'Restaurant 1',
+      description: 'Desc 1',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+    expect(result).toContainEqual({
+      id: 'uuid 2',
+      name: 'Restaurant 2',
+      description: 'Desc 2',
+      status: RestaurantStatus.CLOSED,
+      created: now,
+      updated: now,
+    });
+  });
+
+  it('should get a restaurant by id', async () => {
+    const now = new Date();
+    jest.spyOn(service, 'getById').mockResolvedValueOnce({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+
+    const result = await controller.getById('uuid');
+
+    expect(service.getById).toHaveBeenLastCalledWith('uuid');
+
+    expect(result).toMatchObject({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+  });
+
+  it('should delete a restaurant', async () => {
+    jest.spyOn(service, 'deleteById');
+
+    await controller.delete('uuid');
+
+    expect(service.deleteById).toHaveBeenLastCalledWith('uuid');
+  });
+
+  it("should update a restaurant's status", async () => {
+    const now = new Date();
+    jest.spyOn(service, 'updateStatus').mockResolvedValueOnce({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+
+    const result = await controller.updateStatus('uuid', RestaurantStatus.OPEN);
+
+    expect(service.updateStatus).toHaveBeenLastCalledWith(
+      'uuid',
+      RestaurantStatus.OPEN,
+    );
+
+    expect(result).toMatchObject({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
   });
 });
--- restaurants/src/restaurants/__test__/restaurants.service.spec.ts
+++ restaurants/src/restaurants/__test__/restaurants.service.spec.ts
@@ -1,18 +1,192 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Restaurant } from '../model/restautant.entity';
+import { RestaurantRepository } from '../restaurant.repository';
+import { RestaurantStatus } from '../restaurant.status';
 import { RestaurantsService } from '../restaurants.service';
+
+const mockRestaurantsRepository = () => ({
+  createRestaurant: jest.fn(),
+  findOne: jest.fn(),
+  search: jest.fn(),
+  softDelete: jest.fn(),
+  save: jest.fn(),
+});
 
 describe('RestaurantsService', () => {
   let service: RestaurantsService;
+  let repository: RestaurantRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RestaurantsService],
+      providers: [
+        RestaurantsService,
+        {
+          provide: RestaurantRepository,
+          useFactory: mockRestaurantsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<RestaurantsService>(RestaurantsService);
+    repository = module.get<RestaurantRepository>(RestaurantRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create a restaurant', async () => {
+    const name = 'Restaurant';
+    const description = 'Some description';
+    jest.spyOn(repository, 'createRestaurant').mockResolvedValueOnce({
+      id: 'uuid',
+      name,
+      description,
+      status: RestaurantStatus.CLOSED,
+      created: new Date(),
+      updated: new Date(),
+    });
+
+    const result: Restaurant = await service.create({
+      name,
+      description,
+    });
+
+    expect(repository.createRestaurant).toHaveBeenCalledWith({
+      name,
+      description,
+    });
+
+    expect(result.id).toBe('uuid');
+    expect(result.name).toBe(name);
+    expect(result.description).toBe(description);
+    expect(result.status).toBe(RestaurantStatus.CLOSED);
+  });
+
+  it('should get all restaurants', async () => {
+    const now = new Date();
+    jest.spyOn(repository, 'search').mockResolvedValueOnce([
+      {
+        id: 'uuid 1',
+        name: 'Restaurant 1',
+        description: 'Desc 1',
+        status: RestaurantStatus.CLOSED,
+        created: now,
+        updated: now,
+      },
+      {
+        id: 'uuid 2',
+        name: 'Restaurant 2',
+        description: 'Desc 2',
+        status: RestaurantStatus.CLOSED,
+        created: now,
+        updated: now,
+      },
+    ]);
+
+    const result = await service.searchBy({ status: RestaurantStatus.CLOSED });
+
+    expect(repository.search).toHaveBeenLastCalledWith({
+      status: RestaurantStatus.CLOSED,
+    });
+
+    expect(result).toHaveLength(2);
+    expect(result).toContainEqual({
+      id: 'uuid 1',
+      name: 'Restaurant 1',
+      description: 'Desc 1',
+      status: RestaurantStatus.CLOSED,
+      created: now,
+      updated: now,
+    });
+    expect(result).toContainEqual({
+      id: 'uuid 2',
+      name: 'Restaurant 2',
+      description: 'Desc 2',
+      status: RestaurantStatus.CLOSED,
+      created: now,
+      updated: now,
+    });
+  });
+
+  it('should get a restaurant by id', async () => {
+    const now = new Date();
+    jest.spyOn(repository, 'findOne').mockResolvedValueOnce({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+
+    const result = await service.getById('uuid');
+
+    expect(repository.findOne).toHaveBeenLastCalledWith('uuid');
+
+    expect(result).toMatchObject({
+      id: 'uuid',
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    });
+  });
+
+  it('should throw NotFoundException when restaurant does not exist', async () => {
+    const id = 'uuid';
+
+    await expect(service.getById(id)).rejects.toThrow(
+      `Restaurant ${id} not found`,
+    );
+  });
+
+  it('should delete a restaurant', async () => {
+    jest
+      .spyOn(repository, 'softDelete')
+      .mockResolvedValueOnce({ affected: 1, raw: {}, generatedMaps: [] });
+
+    await service.deleteById('uuid');
+
+    expect(repository.softDelete).toHaveBeenLastCalledWith('uuid');
+  });
+
+  it('should throw NotFoundException when deleted restaurant does not exist', async () => {
+    const id = 'uuid';
+    jest
+      .spyOn(repository, 'softDelete')
+      .mockResolvedValueOnce({ affected: 0, raw: {}, generatedMaps: [] });
+
+    await expect(service.deleteById(id)).rejects.toThrow(
+      `Restaurant ${id} not found`,
+    );
+  });
+
+  it('should update restaurant status', async () => {
+    const id = 'uuid';
+    const now = new Date();
+    const previousData = {
+      id,
+      name: 'Restaurant',
+      description: 'Description',
+      status: RestaurantStatus.OPEN,
+      created: now,
+      updated: now,
+    };
+    jest.spyOn(repository, 'findOne').mockResolvedValueOnce(previousData);
+
+    const result = await service.updateStatus(id, RestaurantStatus.CLOSED);
+
+    expect(repository.save).toHaveBeenLastCalledWith({
+      ...previousData,
+      status: RestaurantStatus.CLOSED,
+    });
+
+    expect(result.status).toBe(RestaurantStatus.CLOSED);
+  });
+
+  it('should throw NotFoundException when updated restaurant does not exist', async () => {
+    const id = 'uuid';
+
+    await expect(
+      service.updateStatus(id, RestaurantStatus.CLOSED),
+    ).rejects.toThrow(`Restaurant ${id} not found`);
   });
 });
